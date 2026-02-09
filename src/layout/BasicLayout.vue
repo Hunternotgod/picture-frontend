9f5e6ddca070681aa377724c288ae70a2dde8010
Hunternotgod <1215462706@qq.com>
2026-02-10 00:59:23 +0800

    init project

---
+++ src/layout/BasicLayout.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div id="basicLayout">
+    <a-layout style="min-height: 100vh">
+      <a-layout-header :style="headerStyle">Header</a-layout-header>
+      <a-layout-content :style="contentStyle">Content</a-layout-content>
+      <a-layout-footer class="footer" :style="footerStyle">
+        <a href="https://gitee.com/fat-cat-barbecue/picture-backend" target="_blank">
+          云图库 by 肥猫叉烧
+        </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style scoped>
+#basicLayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
