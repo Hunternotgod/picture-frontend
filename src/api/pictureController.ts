7fb7eec5612f92dc18432b8d6d8afe9816e18fee
Hunternotgod <1215462706@qq.com>
2026-02-25 00:06:48 +0800

    feat：图片管理模块业务开发

---
+++ src/api/pictureController.ts
@@ -0,0 +1,157 @@
+// @ts-ignore
+/* eslint-disable */
+import request from '@/request'
+
+/** 删除图片 POST /api/picture/delete */
+export async function deletePictureUsingPost(
+  body: API.DeleteRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseBoolean_>('/api/picture/delete', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 编辑图片（给用户使用） POST /api/picture/edit */
+export async function editPictureUsingPost(
+  body: API.PictureEditRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseBoolean_>('/api/picture/edit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 根据 id 获取图片（仅管理员可用） GET /api/picture/get */
+export async function getPictureByIdUsingGet(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.getPictureByIdUsingGETParams,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePicture_>('/api/picture/get', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  })
+}
+
+/** 根据 id 获取图片（封装类） GET /api/picture/get/Vo */
+export async function getPictureVoByIdUsingGet(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.getPictureVoByIdUsingGETParams,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePictureVo_>('/api/picture/get/Vo', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  })
+}
+
+/** 分页获取图片列表（仅管理员可用） POST /api/picture/list/page */
+export async function listPictureByPageUsingPost(
+  body: API.PictureQueryRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePagePicture_>('/api/picture/list/page', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 分页获取图片列表（封装类） POST /api/picture/list/page/Vo */
+export async function listPictureVoByPageUsingPost(
+  body: API.PictureQueryRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePagePictureVo_>('/api/picture/list/page/Vo', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 返回常用标签分类列表 GET /api/picture/tag_category */
+export async function listPictureTagCategoryUsingGet(options?: { [key: string]: any }) {
+  return request<API.BaseResponsePictureTagCategory_>('/api/picture/tag_category', {
+    method: 'GET',
+    ...(options || {}),
+  })
+}
+
+/** 更新图片（仅管理员可用） POST /api/picture/update */
+export async function updatePictureUsingPost(
+  body: API.PictureUpdateRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseBoolean_>('/api/picture/update', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 上传图片 POST /api/picture/upload */
+export async function uploadPictureUsingPost(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.uploadPictureUsingPOSTParams,
+  body: {},
+  file?: File,
+  options?: { [key: string]: any }
+) {
+  const formData = new FormData()
+
+  if (file) {
+    formData.append('file', file)
+  }
+
+  Object.keys(body).forEach((ele) => {
+    const item = (body as any)[ele]
+
+    if (item !== undefined && item !== null) {
+      if (typeof item === 'object' && !(item instanceof File)) {
+        if (item instanceof Array) {
+          item.forEach((f) => formData.append(ele, f || ''))
+        } else {
+          formData.append(ele, new Blob([JSON.stringify(item)], { type: 'application/json' }))
+        }
+      } else {
+        formData.append(ele, item)
+      }
+    }
+  })
+
+  return request<API.BaseResponsePictureVo_>('/api/picture/upload', {
+    method: 'POST',
+    params: {
+      ...params,
+    },
+    data: formData,
+    requestType: 'form',
+    ...(options || {}),
+  })
+}
